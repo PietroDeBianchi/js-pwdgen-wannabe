35c40ab299245f0c9b5f8c1ce556cbf41a6721e2
Pietro De Bianchi <pietro.debianchi96@gmail.com>
2023-02-21 16:07:05 +0100

    wrote the java-code and add some text style

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+const myName = prompt("What is your name?");
+const mySurname = prompt("What is your surname?");
+const myFavoriteColor = prompt("What is your favorite color?");
+
+// this is only for console-inspector check
+console.log(`Your name is ${myName}, your surname is ${mySurname}, and your favorite color is ${myFavoriteColor}.`);
+
+// this is what appear on the browser
+const userInfoElement = document.getElementById("user-info");
+userInfoElement.textContent = `Hello my name is ${myName} ${mySurname}, and your favorite color is ${myFavoriteColor}.`;
